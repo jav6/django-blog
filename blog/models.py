56d978b738ff358cb41e6c5ef59a05604c213b73
Mohammad Javidi <m.javidii@yahoo.com>
2021-07-21 21:54:40 +0430

    Add - model to create table for blog comments

--- blog/models.py
+++ blog/models.py
@@ -6,3 +6,11 @@ class Post(models.Model):
     post_body = models.TextField()
     def __str__(self):
         return self.post_title
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author =  models.CharField(max_length=80)
+    email = models.EmailField()
+    text = models.TextField()
+    def __str__(self):
+        return 'Comment {} by {}'.format(self.text, self.author)
