8fb345f0dd5aa8ca550b1361fde0e48a2fab4612
Mohammad Javidi <m.javidii@yahoo.com>
2021-07-22 23:05:21 +0430

    Add comment system

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Post
+from .models import Post, Comment
 
 def index(request):
     latest_post_list = Post.objects.order_by('-post_title')[:5]
@@ -9,5 +9,9 @@ def index(request):
 
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    context = {'post':post}
+    try:
+        comment = Comment.objects.get(pk=post_id)
+    except:
+        comment = False
+    context = {'post':post, 'comment':comment}
     return render(request, 'blog/detail.html', context)
