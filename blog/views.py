5780e94a5560e4f0f437ff10e0984fa1f42a35ab
Mohammad Javidi <m.javidii@yahoo.com>
2021-07-25 19:44:33 +0430

    Create logic of comment system for blog

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Post, Comment
+from .models import Post
+from .forms import CommentForm
 
 def index(request):
     latest_post_list = Post.objects.order_by('-post_title')[:5]
@@ -9,5 +10,18 @@ def index(request):
 
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
-    context = {'post':post}
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    context ={
+        'post':post,
+        'new_comment': new_comment,
+        'comment_form': comment_form,
+        }
     return render(request, 'blog/detail.html', context)
