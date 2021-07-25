5da0e76a699199a90c17f9338175a21265d869fe
Mohammad Javidi <m.javidii@yahoo.com>
2021-07-25 18:56:04 +0430

    Create - List display for comments

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
+from django.db import models
+from django.db.models.base import Model
 
 from .models import Comment, Post
 
 admin.site.register(Post)
-admin.site.register(Comment)
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['author', 'email', 'post']
