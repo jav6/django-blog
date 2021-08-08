00d00cd95c051542f867cc465f1b90a9dc7ac5dd
Mohammad Javidi <m.javidii@yahoo.com>
2021-08-08 15:00:36 +0430

    Amend - forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,9 @@
 from django import forms
-from django.db import models
-from django.db.models import fields
 
 from .models import Comment
 
+# Create class for add meta to field for form --> from database (models)
 class CommentForm(forms.ModelForm):
-    # Create meta class for add field for form - from database (models)
     class Meta:
         model = Comment
         fields = ('author', 'email', 'text')
