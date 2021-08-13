63d6c31ab49774f84e1277a8f4856c7c8ca3e0df
Mohammad Javidi <m.javidii@yahoo.com>
2021-08-13 18:35:57 +0430

    Repalce - index-view:function with generic class

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
 
 from .models import Post
 from .forms import CommentForm
 
-def index(request):
-    latest_post_list = Post.objects.order_by('-post_title')[:5]
-    context = {'latest_post_list':latest_post_list}
-    return render(request, 'blog/index.html', context)
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    paginate_by = 6
 
 def detail(request, post_id):
     post = get_object_or_404(Post, pk=post_id)
