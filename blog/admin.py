26e705e957e93a77eac387bf1c4f43bafd3eef4d
Mohammad Javidi <m.javidii@yahoo.com>
2021-08-14 22:58:53 +0430

    Register - Image table to admin:panel for manage it

--- blog/admin.py
+++ blog/admin.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.db import models
 from django.db.models.base import Model
 
-from .models import Comment, Post
+from .models import Comment, Image, Post
 
 admin.site.register(Post)
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['author', 'email', 'post']
+admin.site.register(Image)
