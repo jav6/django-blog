50573b0616c6b1a734ed1ca2864e09ef1c4b302e
Mohammad Javidi <m.javidii@yahoo.com>
2021-08-15 11:32:42 +0430

    Add - Image uploader under the post creator

--- blog/admin.py
+++ blog/admin.py
@@ -4,14 +4,20 @@ from django.db.models.base import Model
 
 from .models import Comment, Image, Post
 
+class ImageInline(admin.TabularInline):
+    model=Image
+
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     fields = ('post_title', 'post_slug', 'post_img', 'post_body')
     list_display = ('post_title', 'post_slug', 'post_img')
     prepopulated_fields = {'post_slug': ('post_title',)}
+    inlines = [ImageInline]
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'image']
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['author', 'email', 'post']
-
-admin.site.register(Image)
